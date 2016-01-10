a87af534fd2e0a41b20579c18ac9033dfbaba53b
Rajith Radhakrishnan <radrajith@gmail.com>
2016-01-10 17:10:32 -0500

    created TimerSwitch project

---
+++ TimerSwitch/TimerSwitch.ino
@@ -0,0 +1,48 @@
+int light = D3;     //pin to control the outlet 
+int led = D7;       //pin to turn on the built in led
+bool toggle = true; //variable to keep track of current position of the relay
+void setup() {
+    pinMode(light, OUTPUT);     //pin D3 is initialized as a output pin 
+    pinMode(led, OUTPUT);       //pin D7 is initialized as a output pin. 
+    
+    //Spark.function is used to turn on or off the relay using the internet
+    Spark.function("lightTimer",lightTimer);
+    //The name inside the quotes is the name that will be visible when creating a recipe in IFTTT. This  
+    // will call the lightTimer function. 
+}
+
+void loop() {
+//nothing to loop
+}
+
+int lightTimer(String command){//command will be the string passed when this function is called from IFTTT
+    if(command =="on"){             // turn on the relay
+        toggle = true;
+        digitalWrite(light, HIGH);
+        digitalWrite(led, HIGH);
+        return 1;
+    }
+    else if(command =="off"){       //turn off the relay
+        toggle = false;
+        digitalWrite(light, LOW);
+        digitalWrite(led, LOW);
+        return 0;
+    }
+    else if(command == "toggle"){   //toggle between on and off, using the DO button app. 
+        if(toggle == true){         //turn off the relay since its already on
+            digitalWrite(light, LOW);
+            digitalWrite(led, LOW);
+            toggle = false;
+            return 0;
+        }
+        else{                       //turn on the relay since its already off
+            digitalWrite(light, HIGH); 
+            digitalWrite(led, HIGH);
+            toggle = true;
+            return 1;
+        }
+    }
+    else{                       //other inputs return -1
+        return -1;
+    }
+}
